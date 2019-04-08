73d9224c8e3da070abd97cee53ccc385b0dbcd11
jsmyth <jsmyth@zoomcare.com>
2019-04-08 15:24:13 -0700

    change data structure to be less abstract

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,15 +1,14 @@
-create table node (
+create table employee (
   id bigint(20) not null auto_increment,
-  parent_node_id bigint(20),
+  supervisor_id bigint(20),
   primary key (id),
-  foreign key (parent_node_id) references node(id)
+  foreign key (supervisor_id) references employee(id)
 );
 
 create table property (
-  id bigint(20) not null auto_increment,
-  node_id bigint(20) not null,
+  employee_id bigint(20) not null,
   key varchar(256),
   value varchar(256),
-  primary key (id),
-  foreign key (node_id) references node(id)
+  primary key (employee_id, key),
+  foreign key (employee_id) references employee(id)
 );
--- src/main/resources/db/migration/V2__initial_data_structures.sql
+++ src/main/resources/db/migration/V2__initial_data_structures.sql
@@ -1,39 +1,39 @@
-insert into node values ();
+insert into employee values ();
 select @ceo := scope_identity();
-insert into property (node_id, key, value) values (@ceo, 'title', 'CEO');
+insert into property (employee_id, key, value) values (@ceo, 'title', 'CEO');
 
-insert into node (parent_node_id) values (@ceo);
+insert into employee (supervisor_id) values (@ceo);
 select @vpsales := scope_identity();
-insert into property (node_id, key, value) values (@vpsales, 'title', 'Vice President of Sales');
+insert into property (employee_id, key, value) values (@vpsales, 'title', 'Vice President of Sales');
 
-insert into node(parent_node_id) values (@vpsales);
+insert into employee(supervisor_id) values (@vpsales);
 select @nasales := scope_identity();
-insert into property (node_id, key, value) values (@nasales, 'title', 'Regional Director of Sales');
-insert into property (node_id, key, value) values (@nasales, 'region', 'North America');
+insert into property (employee_id, key, value) values (@nasales, 'title', 'Regional Director of Sales');
+insert into property (employee_id, key, value) values (@nasales, 'region', 'North America');
 
-insert into node(parent_node_id) values (@nasales);
+insert into employee(supervisor_id) values (@nasales);
 select @salesrep1 := scope_identity();
-insert into property (node_id, key, value) values (@salesrep1, 'title', 'Sales Representative');
+insert into property (employee_id, key, value) values (@salesrep1, 'title', 'Sales Representative');
 
-insert into node(parent_node_id) values (@vpsales);
+insert into employee(supervisor_id) values (@vpsales);
 select @eusales := scope_identity();
-insert into property (node_id, key, value) values (@eusales, 'title', 'Regional Director of Sales');
-insert into property (node_id, key, value) values (@eusales, 'region', 'Europe');
+insert into property (employee_id, key, value) values (@eusales, 'title', 'Regional Director of Sales');
+insert into property (employee_id, key, value) values (@eusales, 'region', 'Europe');
 
-insert into node (parent_node_id) values (@ceo);
+insert into employee (supervisor_id) values (@ceo);
 select @vpp := scope_identity();
-insert into property (node_id, key, value) values (@vpp, 'title', 'Vice President of People');
+insert into property (employee_id, key, value) values (@vpp, 'title', 'Vice President of People');
 
-insert into node (parent_node_id) values (@ceo);
+insert into employee (supervisor_id) values (@ceo);
 select @vpm := scope_identity();
-insert into property (node_id, key, value) values (@vpm, 'title', 'Vice President of Marketing');
+insert into property (employee_id, key, value) values (@vpm, 'title', 'Vice President of Marketing');
 
-insert into node(parent_node_id) values (@vpm);
+insert into employee(supervisor_id) values (@vpm);
 select @namarketing := scope_identity();
-insert into property (node_id, key, value) values (@namarketing, 'title', 'Regional Director of Marketing');
-insert into property (node_id, key, value) values (@namarketing, 'region', 'North America');
+insert into property (employee_id, key, value) values (@namarketing, 'title', 'Regional Director of Marketing');
+insert into property (employee_id, key, value) values (@namarketing, 'region', 'North America');
 
-insert into node(parent_node_id) values (@vpm);
+insert into employee(supervisor_id) values (@vpm);
 select @eumarketing := scope_identity();
-insert into property (node_id, key, value) values (@eumarketing, 'title', 'Regional Director of Marketing');
-insert into property (node_id, key, value) values (@eumarketing, 'region', 'Europe');
+insert into property (employee_id, key, value) values (@eumarketing, 'title', 'Regional Director of Marketing');
+insert into property (employee_id, key, value) values (@eumarketing, 'region', 'Europe');
