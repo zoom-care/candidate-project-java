d9137732cd1fab0b9ea5b52a1f45f8b564a2fc99
Donavan Anderson <donavan@dnvn.us>
2019-11-12 16:02:09 -0800

    Added implementation to meet requirements.

--- src/main/java/com/zoomcare/candidatechallenge/controller/EmployeeController.java
+++ src/main/java/com/zoomcare/candidatechallenge/controller/EmployeeController.java
@@ -11,17 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// The database is a relational database containing two tables, EMPLOYEE and PROPERTIES. The
-// EMPLOYEE table is a simple mapping of an employee's id to their supervisor's id (or null if
-// they have no supervisor). The PROPERTIES table contains a map of key/value pairs of properties
-// for the employee.
-//
-// The resulting services should allow users to get a list of all top-level employees or to
-// specify an employee by id to return just that employee. The result for each employee, whether
-// in the top level list or an individual, should include the employee's Id and all properties as
-// well as a nested list of all direct reports for that employee. This applies any where in the
-// structure an employee is displayed, creating a deeply nested structure of the organization.
-
 /**
  * This class is the main Employee controller.
  */
