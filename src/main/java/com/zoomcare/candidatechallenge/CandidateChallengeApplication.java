50d9143f0ef4f19594bad85ecd8c73344f73db0a
Mortdestro <danmalear@gmail.com>
2020-03-07 14:55:48 -0800

    Added debugging code for Spring Boot/H2 interfacing

--- src/main/java/com/zoomcare/candidatechallenge/CandidateChallengeApplication.java
+++ src/main/java/com/zoomcare/candidatechallenge/CandidateChallengeApplication.java
@@ -1,11 +1,31 @@
 package com.zoomcare.candidatechallenge;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class CandidateChallengeApplication
 {
+	@RequestMapping("/")
+	String home() {
+		EmployeeController controller = new EmployeeController();
+		List<Employee> employees = controller.getAll();
+		if (employees == null) {
+			return "employees is null";
+		}
+		Employee employee = employees.get(0);
+		if (employee == null) {
+			return "employee is null";
+		}
+		return "Employee ID: " + employee.getId() + ", Supervisor ID: " + employee.getSupervisorId();
+	}
 
 	public static void main(String[] args)
 	{
