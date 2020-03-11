def6ae1979ca516613601824e90f99de1178e65a
Nano <ladyrebecca@protonmail.com>
2020-03-11 15:52:06 -0700

    Fixed bug where direct reports was null in DB which resulted in invalid direct reports being added

--- src/main/java/com/zoomcare/candidatechallenge/EmployeeController.java
+++ src/main/java/com/zoomcare/candidatechallenge/EmployeeController.java
@@ -19,75 +19,82 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class EmployeeController {
-	  @GetMapping("/EMPLOYEE")
-	  @ResponseBody
-	  public List<Employee> lookupEmployee(@RequestParam(name="id", required=false) String employeeId) {
-		  Long employeeIdLong = null;
-		  try
-		  {
-		   employeeIdLong = Long.parseLong(employeeId);
-		  }
-	  	 catch (NumberFormatException e) {  
-	         // Invalid number, return top level employees instead
-	      }  
-		  final Long innerEmployeeIdLong = employeeIdLong == null ? null : new Long(employeeIdLong);
-		  // Top level employees are defined as employees which have a SUPERVISOR_ID value of null.
-		  String baseQuery = "SELECT E.ID, E.SUPERVISOR_ID, P.KEY, P.VALUE, DR.ID AS DIRECTREPORT FROM EMPLOYEE E LEFT JOIN EMPLOYEE DR ON E.ID = DR.SUPERVISOR_ID LEFT JOIN PROPERTY P ON E.ID = P.EMPLOYEE_ID WHERE ";
-		  String whereClause = (employeeIdLong == null) ? "E.SUPERVISOR_ID IS NULL" : "E.ID = ?";
-		  
-		  return jdbcTemplate.query(
-					        baseQuery.concat(whereClause),
-					        new PreparedStatementSetter() {
-								@Override
-								public void setValues(PreparedStatement ps) throws SQLException {
-									if (innerEmployeeIdLong != null)
+	@GetMapping("/API/V2/EMPLOYEE")
+	@ResponseBody
+	public List<Employee> lookupEmployee(@RequestParam(name="id", required=false) String employeeId) {
+		Long employeeIdLong = null;
+		try
+		{
+			employeeIdLong = Long.parseLong(employeeId);
+		}
+		catch (NumberFormatException e) {  
+			// Invalid number, return top level employees instead
+		}  
+		final Long innerEmployeeIdLong = employeeIdLong == null ? null : new Long(employeeIdLong);
+		// Top level employees are defined as employees which have a SUPERVISOR_ID value of null.
+		String baseQuery = "SELECT E.ID, E.SUPERVISOR_ID, P.KEY, P.VALUE, DR.ID AS DIRECTREPORT FROM EMPLOYEE E LEFT JOIN EMPLOYEE DR ON E.ID = DR.SUPERVISOR_ID LEFT JOIN PROPERTY P ON E.ID = P.EMPLOYEE_ID WHERE ";
+		String whereClause = (employeeIdLong == null) ? "E.SUPERVISOR_ID IS NULL" : "E.ID = ?";
+
+		return jdbcTemplate.query(
+				baseQuery.concat(whereClause),
+				new PreparedStatementSetter() {
+					@Override
+					public void setValues(PreparedStatement ps) throws SQLException {
+						if (innerEmployeeIdLong != null)
+						{
+							ps.setLong(1, innerEmployeeIdLong);
+						}
+					}},
+				new ResultSetExtractor<List<Employee>>(){
+						@Override
+						public List<Employee> extractData(ResultSet rs)
+								throws SQLException, DataAccessException {
+							List<Employee> employeeList = new ArrayList<Employee>();
+							Long extEmployeeId = null;
+							Long extSupervisorId = null;
+							List<Long> extDirectReports = new ArrayList<Long>();
+							HashMap<String, String> extKeyValues = new HashMap<String, String>();
+							while(rs.next())
+							{ 
+								if (extEmployeeId != null && extEmployeeId.longValue() != rs.getLong("ID"))
+								{
+									// create a new Employee pojo from what has been extracted already
+									employeeList.add(new Employee(extEmployeeId, extSupervisorId, extDirectReports, extKeyValues));
+									extEmployeeId = null;
+								}
+								if (extEmployeeId == null)
+								{
+									extEmployeeId = rs.getLong("ID");
+									extSupervisorId = rs.getLong("SUPERVISOR_ID");
+									if (rs.wasNull())
 									{
-									ps.setLong(1, innerEmployeeIdLong);
+										extSupervisorId = null;
 									}
-								}},
-					        new ResultSetExtractor<List<Employee>>(){
-								@Override
-								public List<Employee> extractData(ResultSet rs)
-										throws SQLException, DataAccessException {
-									List<Employee> employeeList = new ArrayList<Employee>();
-									Long extEmployeeId = null;
-									Long extSupervisorId = null;
-									List<Long> extDirectReports = new ArrayList<Long>();
-									HashMap<String, String> extKeyValues = new HashMap<String, String>();
-									while(rs.next())
-									{ 
-										if (extEmployeeId != null && extEmployeeId.longValue() != rs.getLong("ID"))
-										{
-											// create a new Employee pojo from what has been extracted already
-											employeeList.add(new Employee(extEmployeeId, extSupervisorId, extDirectReports, extKeyValues));
-											extEmployeeId = null;
-										}
-										if (extEmployeeId == null)
-										{
-											extEmployeeId = rs.getLong("ID");
-											extSupervisorId = rs.getLong("SUPERVISOR_ID");
-										}
-										
-										if (extEmployeeId != null && extEmployeeId.longValue() == rs.getLong("ID") )
-										{
-											// extract properties and direct reports
-											String key = rs.getString("KEY");
-											String value = rs.getString("VALUE");
-											extKeyValues.put(key, value);
-											Long directReport = rs.getLong("DIRECTREPORT");
-											extDirectReports.add(directReport);
-										}
-										if (rs.isLast())
-										{
-											// create a new Employee pojo from what has been extracted already
-											employeeList.add(new Employee(extEmployeeId, extSupervisorId, extDirectReports, extKeyValues));
-										}
+								}
+
+								if (extEmployeeId != null && extEmployeeId.longValue() == rs.getLong("ID") )
+								{
+									// extract properties and direct reports
+									String key = rs.getString("KEY");
+									String value = rs.getString("VALUE");
+									extKeyValues.put(key, value);
+									Long directReport = rs.getLong("DIRECTREPORT");
+									if (!rs.wasNull())
+									{
+										extDirectReports.add(directReport);
 									}
-									return employeeList;
-								}    	  
-					         });
-	  }
-	  
-	  @Autowired
-	  JdbcTemplate jdbcTemplate;
+								}
+								if (rs.isLast())
+								{
+									// create a new Employee pojo from what has been extracted already
+									employeeList.add(new Employee(extEmployeeId, extSupervisorId, extDirectReports, extKeyValues));
+								}
+							}
+							return employeeList;
+						}    	  
+					});
+	}
+
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 }
