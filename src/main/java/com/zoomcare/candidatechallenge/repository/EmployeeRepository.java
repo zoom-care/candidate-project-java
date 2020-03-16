18e005488b18406b89d99c72f8251cff67cd4d98
Mitch-Lu <mitchell.lu@outlook.com>
2020-03-16 02:26:25 -0700

    minor

--- src/main/java/com/zoomcare/candidatechallenge/repository/EmployeeRepository.java
+++ src/main/java/com/zoomcare/candidatechallenge/repository/EmployeeRepository.java
@@ -3,11 +3,13 @@ package com.zoomcare.candidatechallenge.repository;
 import com.zoomcare.candidatechallenge.model.Employee;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface EmployeeRepository extends CrudRepository<Employee, Long>
 {
+    @Transactional(readOnly = true)
     @Query("SELECT distinct e FROM Employee e " +
             "LEFT JOIN e.employees LEFT JOIN e.properties WHERE e.supervisor IS NULL ")
     List<Employee> findAllTopLevelEmployees();
