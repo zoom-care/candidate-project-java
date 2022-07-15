22bb08af053919a11db4e2eee3351dd4e4e976cc
Sergio de la Torre <sergio.delatorre@unmsm.edu.pe>
2022-07-14 21:32:28 -0500

    fix: Resource not found exception handler

--- src/main/java/com/zoomcare/candidatechallenge/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/zoomcare/candidatechallenge/service/impl/EmployeeServiceImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.zoomcare.candidatechallenge.dto.EmployeeAllDto;
+import com.zoomcare.candidatechallenge.exception.ResourceNotFoundException;
 import com.zoomcare.candidatechallenge.model.Employee;
 import com.zoomcare.candidatechallenge.repository.EmployeeRepository;
 import com.zoomcare.candidatechallenge.service.EmployeeService;
@@ -20,7 +21,8 @@ public class EmployeeServiceImpl implements EmployeeService{
 
 	@Override
 	public EmployeeAllDto getEmployeeById(Long employeeId) {
-		Employee employee = employeeRepository.findById(employeeId).orElse(new Employee());
+		Employee employee = employeeRepository.findById(employeeId).orElseThrow(
+				() -> new ResourceNotFoundException("Employee not found"));
 		return mapper.map(employee, EmployeeAllDto.class);
 	}
 
