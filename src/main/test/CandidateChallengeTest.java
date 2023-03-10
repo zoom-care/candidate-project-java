0e86f0cb311b20cd71d51ae99e00dd38a33586b4
Giovanny Quevedo <giovanny.quevedo@softvision.com>
2023-03-09 18:18:21 -0600

    Major changes including testUnit

---
+++ src/main/test/CandidateChallengeTest.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CandidateChallengeTest {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
